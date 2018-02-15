1fc8bbd188184a0089a377c860025ebd0637ed46
Eduardo Sousa <eduardosousa@av.it.pt>
2018-02-15 17:44:23 +0000

    Revoking when not authentication comes from service

--- auth.py
+++ auth.py
@@ -2,9 +2,14 @@ from flask import Flask, request, make_response, abort, jsonify
 
 from basic_authentication import BasicAuthentication
 from token_authentication import TokenAuthentication
+from token_validation import TokenValidator
+from token_revokation import TokenRevokator
 from user import User
+from settings import Config
+
 
 app = Flask(__name__)
+
 
 @app.route('/auth', methods=['GET'])
 def auth_get():
@@ -29,30 +34,47 @@ def auth_get():
 def auth_post():
     """Method to authenticate users (using credentials or tokens)"""
     req_data = request.get_json()
-    resp_data = None
-    gen_token = None
     auth_method = req_data.get('method')
 
+    resp_data = None
+    gen_token = None
+
     if auth_method == 'password':
+        serv_token = request.headers.get('X-Service-Token')
         auth_user = req_data.get('user')
         auth_pass = req_data.get('password')
 
-        if auth_user is not None and auth_pass is not None:
-            user = BasicAuthentication().authenticate(
-                username=auth_user,
-                password=auth_pass
-            )
+        if auth_user is not None and auth_pass is not None:    
+            user = BasicAuthentication().authenticate(username=auth_user,password=auth_pass)
 
             if user is None:
                 return make_response('user not found.', 404)
 
-            gen_token = user.token
-            resp_data = user.to_public_dict()
+            if user.is_service is True:
+                gen_token = user.token
+                resp_data = user.to_public_dict()
+            elif serv_token is not None:
+                service = TokenValidator().validate_token(serv_token)
+
+                if service is None or service.is_service is False:
+                    TokenRevokator().revoke_token(user.token)
+                    return make_response('user authentication must be done by service user', 400)
+                else:
+                    if Config.EXTERNAL_MAPPING_VERIFICATION is True:
+                        # TODO: Externally verify user
+                        pass
+                    else:
+                        gen_token = user.token
+                        resp_data = user.to_public_dict()
+            else:
+                TokenRevokator().revoke_token(user.token)
+                return make_response('user authentication must be done by service user', 400)
         elif auth_user is None:
             return make_response('user must be provided.', 400)
         else:
             return make_response('password must be provided.', 400)
     elif auth_method == 'token':
+        serv_token = request.headers.get('X-Service-Token')
         auth_token = req_data.get('token')
 
         if auth_token is not None:
@@ -61,8 +83,25 @@ def auth_post():
             if user is None:
                 return make_response('token not found.', 404)
 
-            gen_token = user.token
-            resp_data = user.to_public_dict()
+            if user.is_service is True:
+                gen_token = user.token
+                resp_data = user.to_public_dict()
+            elif serv_token is not None:
+                service = TokenValidator().validate_token(serv_token)
+
+                if service is None or service.is_service is False:
+                    TokenRevokator().revoke_token(user.token)
+                    return make_response('user authentication must be done by service user', 400)
+                else:
+                    if Config.EXTERNAL_MAPPING_VERIFICATION is True:
+                        # TODO: Externally verify user
+                        pass
+                    else:
+                        gen_token = user.token
+                        resp_data = user.to_public_dict()
+            else:
+                TokenRevokator().revoke_token(user.token)
+                return make_response('user authentication must be done by service user', 400)
         else:
             return make_response('token must be provided.', 400)
     else:
